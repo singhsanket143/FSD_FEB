d070fec6a1bfaceada87d5d4532b1ccb95dd6332
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-05-02 22:45:35 +0530

    Added codes

--- Apr_27/first_express_app/index.js
+++ Apr_27/first_express_app/index.js
@@ -1,7 +1,20 @@
 const express = require('express'); // require the express module which gives us a function 
+const morgan = require('morgan')
 const app = express(); // this creates a new express application object
-
 const port = 3000; // this is the port on which app server will listen
+
+app.use(morgan('tiny')); // if we want to add a middleware for every req then we use app.use
+
+function isSignedIn(req, res, next) {
+    // some piece of code
+    console.log("Yes signed in");
+    next(); // isAuthorized
+}
+
+function isAuthorized(req, res, next) {
+    console.log("Checking if authorized");
+    next(); // the callback function 
+}
 
 app.get('/', (req, res) => {
     // this callback will be executed when we receive any request on /
@@ -9,7 +22,7 @@ app.get('/', (req, res) => {
     res.send('Hello world !!!');
 });
 
-app.get('/home', (req, res) => {
+app.get('/home',isSignedIn, isAuthorized, (req, res) => {
     // this callback will be executed when we receive any request on /home
     console.log('Received a new request on /home');
     res.send('Home page');
@@ -20,3 +33,7 @@ app.listen(port, () => {
     // server starts listening on the port given
     console.log('Server started');
 })
+
+
+// adjust the isSignedIn function in /home such that it gets executed before the 
+// req, res callback
