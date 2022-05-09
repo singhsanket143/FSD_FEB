ca4ef14710214e6b32365864008cce3672c2bd26
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-05-09 22:34:37 +0530

    Added codes

--- Ecommerce_App_Api/server.js
+++ Ecommerce_App_Api/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const configs = require('./configs/serverconfig');
-
+const categoryRoutes = require('./routes/category.routes');
 const app = express();
+
+categoryRoutes(app);
+
+app.get('/home', function (req, res) {
+    res.send("welcome to home");
+})
 
 app.listen(configs.PORT, () => {
     console.log('Server started on PORT', configs.PORT);
