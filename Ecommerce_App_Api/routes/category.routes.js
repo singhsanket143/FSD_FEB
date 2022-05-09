c3d17aff74eb674a2be2b1cb604cec958e755c79
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-05-09 23:01:03 +0530

    Added codes

--- Ecommerce_App_Api/routes/category.routes.js
+++ Ecommerce_App_Api/routes/category.routes.js
@@ -4,4 +4,4 @@ const routes = (app) => { // the app parameter is the express app only
     app.get('/ecomm/api/v1/categories', CategoryController.getCategory);
 }
 
-module.exports = routes;
+module.exports = routes; 
